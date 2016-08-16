583b97985dc8d69288dbd10e80006246a8d110e5
Damien Profeta <damien.profeta@amadeus.com>
2016-08-16 15:56:45 +0200

    First emacs file

---
+++ emacs/emacs.d.symlink/init.el
@@ -0,0 +1,19 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(ido-mode 1)
+
+(setq  make-backup-files nil)
+(fset 'yes-or-no-p 'y-or-n-p)
+(show-parent-mode 1)
+
+#customize calendar
+(setq calendar-week-start-day 1
+      calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'font-lock-function-name-face))

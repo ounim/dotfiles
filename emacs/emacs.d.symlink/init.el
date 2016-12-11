99c52d9e0da2fb11124c02694087b8146ce23770
Damien Profeta <damien@ounim.fr>
2016-12-11 15:19:07 +0100

    Fix zenburn theme

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -31,12 +31,23 @@
 
 (require 'use-package)
 
-(use-package color-theme
-  :ensure t)
-(use-package color-theme-zenburn
+(use-package zenburn-theme
   :ensure t
-  :config
-  (color-theme-zenburn))
+  :init
+  (load-theme 'zenburn t))
 
 (require 'amadeus)
 (put 'narrow-to-region 'disabled nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zenburn-theme color-theme-zenburn))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
